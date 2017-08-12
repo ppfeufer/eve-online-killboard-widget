ce9dc007c044df09b21efb89db94a2349cd6e82f
H.-Peter Pfeufer <info@ppfeufer.de>
2017-08-12 11:05:48 +0200

    Code optimizations

--- Libs/AjaxApi.php
+++ Libs/AjaxApi.php
@@ -29,12 +29,12 @@ class AjaxApi {
 			die('Busted!');
 		} // END if(!\wp_verify_nonce($nonce, 'ajax-nonce-eve-online-killboard-widget'))
 
-		$entityType = \filter_input(\INPUT_POST, 'type');
-		$entityName  = \filter_input(\INPUT_POST, 'name');
-		$numberOfKills = \filter_input(\INPUT_POST, 'count');
-		$showLosses = \filter_input(\INPUT_POST, 'showLosses');
-
-		$killList = \WordPress\Plugin\EveOnlineKillboardWidget\Helper\KillboardHelper::getInstance()->getKillList($entityType, $entityName, $numberOfKills, $showLosses);
+		$killList = \WordPress\Plugin\EveOnlineKillboardWidget\Helper\KillboardHelper::getInstance()->getKillList([
+			'eve-online-killboard-widget-entity-type' => \esc_html(\filter_input(\INPUT_POST, 'type')),
+			'eve-online-killboard-widget-entity-name' => \esc_html(\filter_input(\INPUT_POST, 'name')),
+			'eve-online-killboard-widget-number-of-kills' => \filter_input(\INPUT_POST, 'count'),
+			'eve-online-killboard-widget-show-losses' => \filter_input(\INPUT_POST, 'showLosses')
+		]);
 
 		$widgetHtml = null;
 		if(!empty($killList) && is_array($killList)) {
